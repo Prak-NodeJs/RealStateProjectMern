116e67cb03d60569b431325a1defc8a6324ec5c0
Prakash Prajapati <prakash.sp@accubits.com>
2023-10-23 11:49:24 +0530

    mongo connection added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("conected to mongodb")
+}).catch((err) => {
+    console.log(err)
+})
 
 const app = express()
 app.listen(3000, () => {

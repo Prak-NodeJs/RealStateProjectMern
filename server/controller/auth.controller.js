1d32cacebb502cada1f2cfac3176d414e382127c
Prakash Prajapati <prakash.sp@accubits.com>
2023-10-23 12:38:36 +0530

    errorhandler added

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
-export const signUp = async (req, res) => {
+
+export const signUp = async (req, res, next) => {
     const { username, email, password } = req.body
     const hashedPassword = bcrypt.hashSync(password, 10)
-    console.log(hashedPassword)
     const newUser = new User({ username, email, password: hashedPassword })
     try {
         await newUser.save()
+        res.status(201).json("user created successfully")
 
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
-    res.status(201).json("user created successfully")
 }

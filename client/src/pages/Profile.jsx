16b13f0e3c489e8035b8f124fc90d8b5f126b445
Prakash Prajapati <prakash.sp@accubits.com>
2023-10-26 15:43:42 +0530

    create update listing route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -110,6 +110,19 @@ export default function Profile() {
         }
     }
 
+    const handleListingDelete = async (listingId) => {
+        try {
+            const res = await fetch(`/api/listing/delete/${listingId}`, { method: 'DELETE' })
+            const data = await res.json()
+            if (data.success === false) {
+                console.log(data.message)
+                return
+            }
+            setUserListing((prev) => prev.filter((listing) => listing._id !== listingId))
+        } catch (error) {
+            console.log(error)
+        }
+    }
     const handleShowListings = async () => {
         try {
             const res = await fetch(`/api/user/listings/${currentUser._id}`
@@ -176,7 +189,7 @@ export default function Profile() {
                             </Link>
 
                             <div className="flex items-center flex-col">
-                                <button className='text-red-700 uppercase'>Delete</button>
+                                <button className='text-red-700 uppercase' onClick={() => handleListingDelete(listing._id)}>Delete</button>
                                 <button className='text-green-700 uppercase'>Edit</button>
                             </div>
                         </div>
--- server/controller/listing.controller.js
+++ server/controller/listing.controller.js
@@ -1,4 +1,6 @@
 import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js"
+
 
 export const createListing = async (req, res, next) => {
 
@@ -8,5 +10,41 @@ export const createListing = async (req, res, next) => {
 
     } catch (error) {
         next(error)
+    }
+}
+
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id)
+    if (!listing) {
+        return next(errorHandler(404, 'listing not found'))
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'you can only delete your own listings'))
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id)
+        res.status(200).json('listing has been deleted.')
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id)
+    if (!listing) {
+        return next(errorHandler(404, 'listing not found'))
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'you can only update your own listings'))
+    }
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        res.status(200).json(updatedListing)
+
+    } catch (error) {
+        next(error)
+
     }
 }
